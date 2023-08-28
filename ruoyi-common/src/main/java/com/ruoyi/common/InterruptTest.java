6a3936445a307434b2af37771201d7467b84a4a8
aozeyu <3484766443@qq.com>
2023-08-28 09:09:16 +0800

    add code

---
+++ ruoyi-common/src/main/java/com/ruoyi/common/InterruptTest.java
@@ -0,0 +1,21 @@
+package com.ruoyi.common;
+
+import java.util.concurrent.TimeUnit;
+
+public class InterruptTest {
+    public static void main(String[] args) throws InterruptedException{
+        Thread thread = new Thread(() -> {
+            try {
+                System.out.println("thread interrupt :" + Thread.currentThread().isInterrupted());
+                TimeUnit.SECONDS.sleep(4);
+            }catch (InterruptedException e) {
+                System.out.println("捕捉到异常");
+                System.out.println("thread interrupt :" + Thread.currentThread().isInterrupted());
+            }
+        });
+        System.out.println(thread.isInterrupted());
+        thread.start();
+        TimeUnit.SECONDS.sleep(1);
+        thread.interrupt();
+    }
+}
